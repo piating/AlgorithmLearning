6495335c7915e6ab7a422635cf2e09105ac75a89
Plating <39881514+piating@users.noreply.github.com>
2021-12-08 20:24:04 +0800

    Create DichotomyStudy.java

---
+++ leetcode/DichotomyStudy.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 二分法
+ */
+public class DichotomyStudy {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 400
+     * 给你一个整数 n ，请你在无限的整数序列 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...] 中找出并返回第 n 位数字。
+     *
+     * @return
+     */
+
+}
